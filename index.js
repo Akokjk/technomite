fd077ec401dc3e4d2738d9618d4086d1ccced930
Aaron Barack <abarack@gmail.com>
2018-10-15 14:54:17 -0700

    added change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=> {
-  res.send({hi: 'there'});
+  res.send({bye: 'buddy'});
 });
 //we are going to do this alot
 
