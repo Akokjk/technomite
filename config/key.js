d105d946c24dfda35373d90b9486f4cec1e33568
Aaron Taylor Barack <atbarack@gmail.com>
2018-10-16 14:47:00 -0700

    working on google oauth20

---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+  googleClientID: '980228086425-580409kvpdedq16skopkm9euufl5l6d6.apps.googleusercontent.com',
+  googleClientSecert: '0yqRZljzt2iPcq4TbBzd-6Y6'
+};
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const passport = require('passport');
+const g_strat = require('passport-google-oauth20').Strategy;
+const keys = require('./config/key');
 
 const app = express();
-
-app.get('/', (req, res)=> {
-  res.send({bye: 'buddy'});
-});
+//need to download config to get secrets
+passport.use(new  g_strat({
+  clientID: keys.googleClientID,
+  clientSecert: keys.googleClientSecert,
+  callbackURL: '/auth/google/callback'
+}, (accessToken) =>{
+  console.log(accessToken);
+}
+));
 //we are going to do this alot
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
 
 
 const PORT = process.env.PORT || 5000;
